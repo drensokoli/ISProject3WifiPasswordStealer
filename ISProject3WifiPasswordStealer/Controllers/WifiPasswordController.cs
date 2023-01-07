c12569466c03f0dcf7c95079b325aeded849014e
EraKadiri <78500922+EraKadiri@users.noreply.github.com>
2023-01-07 23:24:44 +0100

    Add project files.

---
+++ ISProject3WifiPasswordStealer/Controllers/WifiPasswordController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ISProject3WifiPasswordStealer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WifiPasswordController : ControllerBase
+    {
+        public WifiPasswordController()
+        {
+        }
+    }
+}
